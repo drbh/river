322a7a8a044c62e6e4e01e2dcbb6e44ed2abcd65
drbh <david.richard.holtz@gmail.com>
2023-12-06 10:16:21 -0500

    feat: only init model once and cleanup

--- river-onnx/src/main.rs
+++ river-onnx/src/main.rs
@@ -1,11 +1,20 @@
 use base64::{engine::general_purpose, Engine as _};
 use futures::{future, SinkExt, StreamExt};
 use rand::Rng;
+use river_onnx::{Args, YOLOTask, YOLOv8};
+use std::sync::Arc;
 use std::{error::Error, rc::Rc};
 use tmq::{dealer, Context};
-use yolov8_rs::{Args, YOLOTask, YOLOv8};
+use tokio::sync::Mutex;
 
-async fn worker(ctx: Rc<Context>, worker_id: u64, backend: String) -> Result<(), Box<dyn Error>> {
+type SharedModel = Arc<Mutex<YOLOv8>>;
+
+async fn worker(
+    ctx: Rc<Context>,
+    worker_id: u64,
+    backend: String,
+    model: SharedModel,
+) -> Result<(), Box<dyn Error>> {
     let mut sock = dealer(&ctx).connect(&backend)?;
 
     println!("Worker {} starting", worker_id);
@@ -35,36 +44,21 @@ async fn worker(ctx: Rc<Context>, worker_id: u64, backend: String) -> Result<(),
         // 2. model support dynamic batch inference, so input should be a Vec
         let xs = vec![image];
 
-        let args = Args {
-            model: String::from("yolov8m.onnx"),
-            source: String::from(""),
-            device_id: 0,
-            trt: false,
-            cuda: false,
-            batch: 1,
-            batch_min: 1,
-            batch_max: 32,
-            fp16: false,
-            task: Some(YOLOTask::Detect),
-            nc: None,
-            nk: None,
-            nm: None,
-            width: Some(480),
-            height: Some(640),
-            conf: 0.3,
-            iou: 0.45,
-            kconf: 0.55,
-            plot: false,
-            profile: true,
-        };
+        // Access the shared model
+        let mut model = model.lock().await;
 
-        // 3. build yolov8 model
-        let mut model = YOLOv8::new(args).unwrap();
         model.summary(); // model info
+
+        // // 3. build yolov8 model
+        // let mut model = YOLOv8::new(args).unwrap();
+        // model.summary(); // model info
 
         // 4. run
         let ys = model.run(&xs).unwrap();
         println!("{:?}", ys);
+
+        // Important: release the lock before sending response
+        drop(model);
 
         let response = vec![identity, client_id, request_id, "response".into()];
         sock.send(response).await?;
@@ -80,10 +74,35 @@ fn main() -> tmq::Result<()> {
         .build()?;
     let tasks = tokio::task::LocalSet::new();
 
+    let args = Args {
+        model: String::from("river-onnx/yolov8m.onnx"),
+        source: String::from(""),
+        device_id: 0,
+        trt: false,
+        cuda: false,
+        batch: 1,
+        batch_min: 1,
+        batch_max: 32,
+        fp16: false,
+        task: Some(YOLOTask::Detect),
+        nc: None,
+        nk: None,
+        nm: None,
+        width: Some(480),
+        height: Some(640),
+        conf: 0.3,
+        iou: 0.45,
+        kconf: 0.55,
+        plot: false,
+        profile: true,
+    };
+
+    let model = Arc::new(Mutex::new(YOLOv8::new(args).unwrap()));
+
     // spawn worker
     tasks.spawn_local(async move {
         let randome_worker_id = rand::thread_rng().gen_range(10_000..100_000);
-        worker(ctx, randome_worker_id, backend)
+        worker(ctx, randome_worker_id, backend, model)
             .await
             .expect("Worker failed");
     });
